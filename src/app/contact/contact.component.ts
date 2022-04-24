36c1cad9b868022efa6eac79193e636d2607486c
klamaj <kri.lamaj@gmail.com>
2022-04-24 15:25:07 +0300

    code ref

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from './contact.service';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -6,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _contactService: ContactService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(obj: any){
+    
+  }
 }
