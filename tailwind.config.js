b143aed8f4aa35429e4a6cae6821c8fd17b1fe6f
klamaj <kri.lamaj@gmail.com>
2022-04-28 21:27:07 +0300

    Huntsman Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'agriexportbg': 'url("assets/img/AgriExportImage.jpg)"',
+        'gbtransbg': 'url("assets/img/AgriExportImage.jpg)"',
+        'laleonessabg': 'url("assets/img/AgriExportImage.jpg)"',
+        'omnigearbg': 'url("assets/img/AgriExportImage.jpg)"',
+        'huntsmanbg': 'url("assets/img/HunstmanStudioImage.webp")'
+      },
       backgroundColor: theme => ({
         ...theme('colors'),
         "primary" : "#e6ebf0",
