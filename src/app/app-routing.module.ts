786d1aea6ddfd178638626c08a82826fcfb989e9
Kristel Lamaj <kri.lamaj@gmail.com>
2022-11-12 15:53:56 +0200

    refactor project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Components
-import { MainComponent } from './main/main.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-
 // Routes
-const routes: Routes = [
-  { path: '', component: MainComponent },
-  { path: '', redirectTo: '/main', pathMatch: 'full'},
-  { path: 'contact', component: ContactComponent },
-  { path: 'about', component: AboutComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { MenuComponent } from './menu/menu.component';
-import { AgriexportComponent } from './agriexport/agriexport.component';
-import { GbComponent } from './gb/gb.component';
-import { LaleonessaComponent } from './laleonessa/laleonessa.component';
-import { OmnigearComponent } from './omnigear/omnigear.component';
-import { HuntsmanComponent } from './huntsman/huntsman.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent,
-    AboutComponent,
-    ContactComponent,
-    MenuComponent,
-    AgriexportComponent,
-    GbComponent,
-    LaleonessaComponent,
-    OmnigearComponent,
-    HuntsmanComponent
-  ],
+    AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
