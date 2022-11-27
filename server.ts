3be1592a12e9acf1a49dfe1b7d4afb415369fa9b
Kristel Lamaj <kri.lamaj@gmail.com>
2022-11-27 10:24:09 +0200

    deploy v1.1.6

--- server.ts
+++ server.ts
@@ -11,7 +11,8 @@ import { AppServerModule } from './src/main.server';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/urallimited/browser');
+  // const distFolder = join(process.cwd(), 'dist/urallimited/browser');
+  const distFolder = join(process.cwd(), '../browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
@@ -38,7 +39,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 8000;
 
   // Start up the Node server
   const server = app();
